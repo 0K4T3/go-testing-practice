22054056ddc6904b096ba2f9b15c3004246355f7
0K4T3 <o.keito317@icloud.com>
2020-08-10 15:46:01 +0900

    update test

--- pkg/fizzbuzz/fizzbuzz_test.go
+++ pkg/fizzbuzz/fizzbuzz_test.go
@@ -2,74 +2,42 @@ package fizzbuzz
 
 import "testing"
 
-func TestGiven1Get1(t *testing.T) {
-    actual := FizzBuzz(1)
-    expected := "1"
+func doTest(input int, expected string, t *testing.T) {
+    actual := FizzBuzz(input)
 
     if actual != expected {
         t.Errorf("actual %v\nexpected %v", actual, expected)
     }
+}
+
+func TestGiven1Get1(t *testing.T) {
+    doTest(1, "1", t)
 }
 
 func TestGiven2Get2(t *testing.T) {
-    actual := FizzBuzz(2)
-    expected := "2"
-
-    if actual != expected {
-        t.Errorf("actual %v\nexpected %v", actual, expected)
-    }
+    doTest(2, "2", t)
 }
 
 func TestGiven3GetFizz(t *testing.T) {
-    actual := FizzBuzz(3)
-    expected := "Fizz"
-
-    if actual != expected {
-        t.Errorf("actual %v\nexpected %v", actual, expected)
-    }
+    doTest(3, "Fizz", t)
 }
 
 func TestGiven5GetBuzz(t *testing.T) {
-    actual := FizzBuzz(5)
-    expected := "Buzz"
-
-    if actual != expected {
-        t.Errorf("actual %v\nexpected %v", actual, expected)
-    }
+    doTest(5, "Buzz", t)
 }
 
 func TestGiven6GetFizz(t *testing.T) {
-    actual := FizzBuzz(6)
-    expected := "Fizz"
-
-    if actual != expected {
-        t.Errorf("actual %v\nexpected %v", actual, expected)
-    }
+    doTest(6, "Fizz", t)
 }
 
 func TestGiven10GetBuzz(t *testing.T) {
-    actual := FizzBuzz(10)
-    expected := "Buzz"
-
-    if actual != expected {
-        t.Errorf("actual %v\nexpected %v", actual, expected)
-    }
+    doTest(10, "Buzz", t)
 }
 
 func TestGiven15GetFizzBuzz(t *testing.T) {
-    actual := FizzBuzz(15)
-    expected := "FizzBuzz"
-
-    if actual != expected {
-        t.Errorf("actual %v\nexpected %v", actual, expected)
-    }
+    doTest(15, "FizzBuzz", t)
 }
 
 func TestGiven30GetFizzBuzz(t *testing.T) {
-    actual := FizzBuzz(30)
-    expected := "FizzBuzz"
-
-    if actual != expected {
-        t.Errorf("actual %v\nexpected %v", actual, expected)
-    }
+    doTest(30, "FizzBuzz", t)
 }
